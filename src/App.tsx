1da21c3dfecbda05108cf444e21d02ddeb0d2b80
Ever-QN <QuynhTNgo@live.com>
2023-09-24 14:26:03 -0600

    Fix tailwind functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+// import { Routes, Route } from 'react-router-dom';
+import './index.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
-  )
+    <h1 className='text-3xl font-bold underline'>
+      Hi
+    </h1>
+  );
 }
 
 export default App
