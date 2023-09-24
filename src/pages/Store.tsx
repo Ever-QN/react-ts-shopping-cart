7719a35359b2b3ffa01f6acd4212a1a772f54e24
Ever-QN <QuynhTNgo@live.com>
2023-09-24 16:00:14 -0600

    display store items from fakestoreapi

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react"
+
 export default function Store() {
+
+    const [products, setProducts] = useState<Array<products>>([]);
+
+    type products = {
+        category: string;
+        description: string;
+        id: number;
+        image: string;
+        price: number;
+        rating: object;
+        title: string;
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products');
+                if (!response.ok) {
+                    throw new Error('Network response error');
+                }
+                const data = await response.json();
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching the data: ', error);
+            }
+        }
+
+        fetchData();
+    }, []);
+
     return (
-        <h1>Store</h1>
+        <div className='container'>
+            <h1 className='text-xl font-bold underline mb-4'>Store</h1>
+            <ul className='flex-col justify-center'>
+                {products.map((product) => 
+                <li key={product.id} className='border-2'>
+                    {product.title}, ${product.price}
+                    <img src={product.image} className='w-12' />
+                </li>)}
+            </ul>
+        </div>
     )
 }
