aff40d2b5314977ed7b9e31ccfc1e67a17fe811f
Ever-QN <QuynhTNgo@live.com>
2023-09-30 14:11:34 -0600

    Add openCart, closeCart, cartQuantity and cartItems in ShoppingCartContext type

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -10,10 +10,14 @@ type CartItem = {
 }
 
 type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[]
 }
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
