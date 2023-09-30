7d748b4c311ccff7a0ba39de51a03db6c2f2cc79
Ever-QN <QuynhTNgo@live.com>
2023-09-30 13:46:14 -0600

    Format conditional btn rendering and line up cart text correctly

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -20,7 +20,7 @@ export default function StoreItem({
     title
 }: StoreItemProps) {
 
-    const quantity = 0; // Placeholder to test add to cart btn
+    const quantity = 1; // Placeholder to test add to cart btn
 
     return (
         <div className='card h-100 max-h-full p-4 overflow-hidden'>
@@ -33,20 +33,20 @@ export default function StoreItem({
                     <span className='ml-2 text-lg'>{formatCurrency(price)}</span>
                 </div>
                 <div className='mt-auto'>
-                    { quantity === 0 ? (
+                    {quantity === 0 ? (
                         <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+ Add to Cart</button>
                     ) : (
-                    <div className='flex items-center flex-col gap-2 border-orange-500 border-2'>
-                        <div className='flex items-center justify-center gap-2 border-orange-500 border-2'>
-                            <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>-</button>
-                            <div className=''>
-                                <span className='text-3xl'>{quantity}</span> in cart
+                        <div className='flex items-center flex-col gap-2 border-orange-500 border-2'>
+                            <div className='flex items-center justify-center gap-2 border-orange-500 border-2'>
+                                <button className=' bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>-</button>
+                                <div className=''>
+                                    <span className='text-3xl'>{quantity}</span> in cart
+                                </div>
+                                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
                             </div>
-                            <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
-                        </div>
-                        <button className=' bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button>
-                    </div> 
-                    )}
+                            <button className=' bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button>
+                        </div> 
+                        )}
                 </div>
             </div>
         </div>
