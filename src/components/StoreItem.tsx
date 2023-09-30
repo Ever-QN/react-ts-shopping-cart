d4615b5e2a52b39cabc388587b7abbfb00ef4796
Ever-QN <QuynhTNgo@live.com>
2023-09-30 13:42:53 -0600

    Create add to cart btn, - btn, + btn, remove btn and styles

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -23,7 +23,7 @@ export default function StoreItem({
     const quantity = 0; // Placeholder to test add to cart btn
 
     return (
-        <div className='card h-96 max-h-full border-orange-500 border-2 p-4 overflow-hidden'>
+        <div className='card h-100 max-h-full p-4 overflow-hidden'>
             <div className='card-img flex justify-center mb-4'>
                 <img className='w-56 h-56 object-contain ' src={image} />
             </div>
@@ -33,7 +33,20 @@ export default function StoreItem({
                     <span className='ml-2 text-lg'>{formatCurrency(price)}</span>
                 </div>
                 <div className='mt-auto'>
-                    
+                    { quantity === 0 ? (
+                        <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+ Add to Cart</button>
+                    ) : (
+                    <div className='flex items-center flex-col gap-2 border-orange-500 border-2'>
+                        <div className='flex items-center justify-center gap-2 border-orange-500 border-2'>
+                            <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>-</button>
+                            <div className=''>
+                                <span className='text-3xl'>{quantity}</span> in cart
+                            </div>
+                            <button className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>+</button>
+                        </div>
+                        <button className=' bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>Remove</button>
+                    </div> 
+                    )}
                 </div>
             </div>
         </div>
